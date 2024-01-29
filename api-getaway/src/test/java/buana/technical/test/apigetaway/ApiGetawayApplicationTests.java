0a751c11099bc927a6b598123f0dea0d683ecdf1
rafiatha09 <rafiatha25@gmail.com>
2024-01-30 00:30:25 +0700

    setup microservice architecture

---
+++ api-getaway/src/test/java/buana/technical/test/apigetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package buana.technical.test.apigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/buana/technical/test/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package buana.technical.test.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

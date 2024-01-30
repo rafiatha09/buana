90ec6bf86a627356dc8c99ea927afd5af7019000
rafiatha09 <rafiatha25@gmail.com>
2024-01-30 09:05:22 +0700

    create inventory service model

---
+++ inventory-service/src/main/java/buana/technical/test/inventoryservice/model/Inventory.java
@@ -0,0 +1,36 @@
+package buana.technical.test.inventoryservice.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idInventory;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "location", nullable = false)
+    private String location;
+
+
+    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
+}
---
+++ inventory-service/src/main/java/buana/technical/test/inventoryservice/model/Product.java
@@ -0,0 +1,40 @@
+package buana.technical.test.inventoryservice.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @NotNull
+    @Column(name = "quantity", nullable = false)
+    private BigDecimal quantity;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_inventory", referencedColumnName = "idInventory")
+    private Inventory inventory;
+}
